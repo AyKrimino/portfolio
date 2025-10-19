fe34cb3e9d012993c2b0c1be207fc2e61c0c3d83
AyKrimino <ayoub.krimi14@gmail.com>
2025-10-19 15:17:57 +0100

    fix: make the resume.pdf path relative

--- src/components/ResumeSection.tsx
+++ src/components/ResumeSection.tsx
@@ -12,7 +12,7 @@ const ResumeSection = () => {
     >
       <h2 className="text-3xl font-bold text-center mb-6">Resume</h2>
 
-      <a href="/resume.pdf" download="Ayoub_Krimi_Resume.pdf">
+      <a href="resume.pdf" download="Ayoub_Krimi_Resume.pdf">
         <Button variant="outline" className="">
           Download Resume
         </Button>
