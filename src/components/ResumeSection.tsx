3861dd209672bb158c972f0c71bf0f6dc1d3668a
AyKrimino <ayoub.krimi14@gmail.com>
2025-11-16 18:22:01 +0100

    feat(ui): enhance ui for download resume button

--- src/components/ResumeSection.tsx
+++ src/components/ResumeSection.tsx
@@ -11,7 +11,7 @@ const ResumeSection = () => {
       <h2 className="text-3xl font-bold text-center mb-6">Resume</h2>
 
       <a href="resume.pdf" download="Ayoub_Krimi_Resume.pdf">
-        <Button variant="outline" className="">
+        <Button className="px-6 py-2 text-sm md:text-base bg-gradient-to-r from-gray-900/80 to-gray-900/100 hover:cursor-pointer hover:ring-2 hover:ring-gray-900 hover:scale-105 transition-all duration-200 ease-out shadow-sm hover:shadow-md rounded-lg">
           Download Resume
         </Button>
       </a>
